606cf7e983926297a461cf5a5d6760f5c45dafd0
Yuki Watanabe <47182350+yuki0920@users.noreply.github.com>
2026-04-26 07:13:37 +0900

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,31 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # 開始時間でソートし、未処理のリストに入れる
+        # 同じミーティングループで対応できるものは処理し、できないものは再びリストに入れる
+        intervals.sort(key=lambda i: i.start)
+        ans = 0
+
+        while intervals:
+            new_intervals = []
+            prev_end = 0
+            for interval in intervals:
+                if interval.start < prev_end:
+                    new_intervals.append(interval)
+                else:
+                    prev_end = interval.end
+
+            intervals = new_intervals
+            ans += 1
+
+        return ans
+
+
+
