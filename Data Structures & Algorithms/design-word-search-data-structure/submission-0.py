9353185887b948bc651f1847761c389f62af0ce9
Yuki Watanabe <47182350+yuki0920@users.noreply.github.com>
2026-04-27 10:31:03 +0900

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,41 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        # .に対応するため、DFSを使う
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                # .だった場合、すべてのchildren(value)を検証する
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+
+            return cur.word
+
+        return dfs(0, self.root)
+        
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = True
