4c7858ebf1a0a3ac8998ebbf712c961202d80d3e
Yuki Watanabe <47182350+yuki0920@users.noreply.github.com>
2026-04-28 11:28:02 +0900

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.py
@@ -0,0 +1,32 @@
+class CountSquares:
+
+    def __init__(self):
+        # tupleで個数を保持する
+        self.point_count = defaultdict(int)
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.point_count[tuple(point)] += 1
+        self.points.append(point)
+        
+
+    def count(self, point: List[int]) -> int:
+        # 始点
+        px, py = point
+        
+        ans = 0
+        # 対角線上の点を探し、見つかったら、その両辺の点の個数を追加する(なければ0が追加される)
+        for x, y in self.points:
+            if abs(px - x) != abs(py - y):
+                continue
+            if px == x or py == y:
+                continue
+
+            # 対角線上の点以外の2つの点は、(x, py), (px, y)
+            ans += self.point_count[(x, py)] * self.point_count[(px, y)]
+
+        return ans
+            
+
+            
+        
